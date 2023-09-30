be1e62f9405f4dd795eca04c9baffa8e68380cb6
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-10-01 02:30:31 +0800

    minor changes

--- home/dot_config/lvim/config.lua
+++ home/dot_config/lvim/config.lua
@@ -6,9 +6,11 @@
 -- vim config
 vim.opt.relativenumber = true
 
+-- general
 lvim.format_on_save.enabled = false
 lvim.transparent_window = true
 
+-- keybindings
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
@@ -19,17 +21,15 @@ lvim.keys.normal_mode["<C-o>"] = "<C-i>"
 lvim.builtin.which_key.mappings["-"] = { "<cmd>sp<cr>", "Split Panel" }
 lvim.builtin.which_key.mappings["_"] = { "<cmd>vsp<cr>", "Split Panel Vertically" }
 
-
-lvim.builtin.treesitter.highlight.enable = true
-lvim.builtin.treesitter.autotag.enable = true
-
-
+-- lsp keybindings
 lvim.lsp.buffer_mappings.normal_mode['gr'] = { "<cmd>Telescope lsp_references<cr>", "Goto reference" }
 lvim.lsp.buffer_mappings.normal_mode['gd'] = { "<cmd>Telescope lsp_definitions<cr>", "Goto definition" }
 
+-- treesitter
+lvim.builtin.treesitter.highlight.enable = true
+lvim.builtin.treesitter.autotag.enable = true
 
-
-
+-- neovim plugins
 lvim.plugins = {
   -- { "abzcoding/zephyr-nvim" },  -- not working
   -- colorschemes
