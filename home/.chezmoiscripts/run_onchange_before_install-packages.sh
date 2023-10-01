19248af3cd38f3c93510534810aa62efc0d4edcc
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-10-02 01:08:06 +0800

    install lvim after brew install

--- home/.chezmoiscripts/run_onchange_before_install-packages.sh
+++ home/.chezmoiscripts/run_onchange_before_install-packages.sh
@@ -4,4 +4,21 @@
 chezmoi_source_path="$(chezmoi source-path)"
 
 echo "Executing: brew bundle install --file=\"$chezmoi_source_path/../Brewfile\""
+
+# make sure brew is installed (usually installed by setup.sh)
+if [ ! "$(command -v brew)" ]; then
+  echo "brew could not be found, installing brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 brew bundle install --file="$chezmoi_source_path/../Brewfile"
+
+
+# this require neovim is installed which should be done by brew bundle
+# if ~/.local/bin/lvim not exist, install LunarVim:
+if [ ! -f ~/.local/bin/lvim ]; then
+  # install lunarvim
+  LV_BRANCH='release-1.3/neovim-0.9' /bin/bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+else
+  echo "LunarVim already installed"
+fi
