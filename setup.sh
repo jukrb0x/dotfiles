e9c9b419ac19778bc3ff0d92052dbf7c683c3fa1
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-10-02 02:46:05 +0800

    make key input easier

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-GITHUB_USER="jukrb0x"
+# set GITHUB_USER to your github username if you want to use your own dotfiles
+if [ -z "$GITHUB_USER" ]; then
+    GITHUB_USER="jukrb0x"
+fi
 # to run this script, run the following command:
 #   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jukrb0x/dotfiles/main/setup.sh)"
 # or use wget:
 #   /bin/bash -c "$(wget -O- https://raw.githubusercontent.com/jukrb0x/dotfiles/main/setup.sh)"
+# you can set GITHUB_USER to your github username if you want to use your own dotfiles:
+#   /GITHUB_USER=your_github_username bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jukrb0x/dotfiles/main/setup.sh)"
 
 echo "========================"
 echo " Dev environment setup"
@@ -13,8 +18,8 @@ echo "========================"
 echo "Target machine: $(uname -a)"
 # make sure this is a mac with arm64
 if [ "$(uname -a | grep -c "Darwin.*arm64")" -ne 1 ]; then
-  echo "This script is made for mac with arm64, exiting"
-  exit 1
+    echo "This script is made for mac with arm64, exiting"
+    exit 1
 fi
 echo "This script will do the following:"
 echo "  1. install homebrew"
@@ -30,29 +35,32 @@ echo ""
 # let /bin/bash -c can read stdin 'return' key
 # https://stackoverflow.com/questions/226703/how-do-i-prompt-for-yes-no-cancel-input-in-a-linux-shell-script
 while true; do
-  read -p "Press return to continue setup, other keys to exit" yn
-  case $yn in
-    "" ) break;;
-    * ) exit;;
-  esac
+    read -p "Press return to continue setup, other keys to exit: " -n 1 -s yn
+    if [ -z "$yn" ]; then
+        break
+    else
+        echo ""
+        echo "Exiting..."
+        exit 0
+    fi
 done
 
 
 # install homebrew
 # xcode command line tools will be installed automatically if not installed
 if [ ! "$(command -v brew)" ]; then
-  echo "brew could not be found, installing homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "brew could not be found, installing homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  echo "homebrew already installed."
+    echo "homebrew already installed."
 fi
 
 # install chezmoi and clone dotfiles
 if [ ! "$(command -v chezmoi)" ]; then
-  echo "chezmoi could not be found, installing chezmoi with homebrew"
-  brew install chezmoi
+    echo "chezmoi could not be found, installing chezmoi with homebrew"
+    brew install chezmoi
 else
-  echo "chezmoi already installed."
+    echo "chezmoi already installed."
 fi
 
 echo "Executing: chezmoi init --apply $GITHUB_USER"
