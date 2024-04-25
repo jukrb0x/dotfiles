f67230b64459c9c2e3ecde4abc214a025eb4db13
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2024-04-26 01:59:39 +0800

    feat(lvim): add flash.nvim

--- home/dot_config/lvim/config.lua
+++ home/dot_config/lvim/config.lua
@@ -64,5 +64,19 @@ lvim.plugins = {
   -- auto tag closing for tsx
   {
     "windwp/nvim-ts-autotag",
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   }
 }
